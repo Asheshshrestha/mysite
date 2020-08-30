fb22a0303d06fd492fbefcddac90f485cb04d44a
Ashesh Shrestha <asheshshrestha2@gmail.com>
2020-08-30 12:40:15 +0545

    reset password and main confirmation added

--- mywebsite/accounts/urls.py
+++ mywebsite/accounts/urls.py
@@ -10,5 +10,25 @@ from django.contrib.auth.views import (LoginView,
 urlpatterns = [
     path('register/',SignUpView.as_view(),name='register'),
     path('login/',LoginView.as_view(template_name = 'dashboard/pages/account/login.html'),name='login'),
-    path('logout/',LogoutView.as_view(template_name='dashboard/pages/account/logout.html'),name='logout')
+    path('logout/',LogoutView.as_view(template_name='dashboard/pages/account/logout.html'),name='logout'),
+     path('password-reset/',
+         PasswordResetView.as_view(
+             template_name='dashboard/pages/account/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(
+             template_name='dashboard/pages/account/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='dashboard/pages/account/password_reset_conform.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         PasswordResetCompleteView.as_view(
+             template_name='dashboard/pages/account/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+load_dotenv(os.path.join(BASE_DIR,'.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o-1u(-b*fzamgj05g)alz3(p(fkur@^#5hn86s8rvu6%5pprho'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -142,3 +143,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 FAICON_YAML_FILE = 'fontawesome/metadata/icons.yml'
 FAICON_CSS_URL = 'fontawesome/css/all.css'
+
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST_USER = os.getenv("EMAIL")
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT= 587
+EMAIL_USE_TLS=True
+EMAIL_HOST_PASSWORD= os.getenv("PASSWORD")
